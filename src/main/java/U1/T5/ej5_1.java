06ba64dab1f7afdfe983cf947f29c6a30548ebbf
aperezp <alum.aperezp.2021@iesalixar.org>
2021-09-27 21:19:07 +0200

    T5 - ej5_1

--- src/main/java/U1/T5/ej5_1.java
+++ src/main/java/U1/T5/ej5_1.java
@@ -1,4 +1,18 @@
 package U1.T5;
 
+import java.util.Scanner;
+
 public class ej5_1 {
+
+    public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.print("Introduzca el valor: ");
+        int n = teclado.nextInt();
+        if (n > 0)
+            System.out.println("Es positivo");
+        else
+            System.out.println("Es negativo");
+    }
 }
